81ef9d8f2ac68159d625bcafde52cdab99ea5cd4
rasheed-cy <rasheedsalah13@gmail.com>
2026-04-24 22:40:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,27 @@
 from flask import Flask, request
 import requests
 import io
+import os
 
 app = Flask(__name__)
 
+# بيانات البوت الخاصة بك
 BOT_TOKEN = "8620142253:AAH8TFYy219PdSiRaVx6et_WE04alYjXaVk"
 CHAT_ID = "6315170436"
 
 @app.route('/upload', methods=['POST'])
 def upload():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
+    data = request.get_data()
     
-    if 'file' in request.files:
-        file_to_send = request.files['file']
-        requests.post(url, data={"chat_id": CHAT_ID}, files={"document": file_to_send})
-    else:
-        file_data = request.get_data()
-        if file_data:
-            file_to_send = io.BytesIO(file_data)
-            requests.post(url, data={"chat_id": CHAT_ID}, files={"document": ("AL-Rasheed.backup", file_to_send)})
-        else:
-            return "No data received", 400
-
-    return "OK", 200
+    if data:
+        file_io = io.BytesIO(data)
+        # نرسل الملف للتليجرام
+        r = requests.post(url, data={"chat_id": CHAT_ID}, files={"document": ("AL-Rasheed.backup", file_io)})
+        return f"Done: {r.status_code}", 200
+    
+    return "No Data", 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
